6e507760be5767ced0a55b12d5ca9decbbb93082
Anna Fritz <anna_eida@gmx.de>
2025-01-21 14:35:10 +0100

    added & migrated new field 'newsletter_abo'

--- sales/models.py
+++ sales/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     second_name = models.CharField(max_length=30)
-    newsletter_abo = models.BooleanField()
-    email_address = models.EmailField()
-    account = models.FloatField()
+    newsletter_abo = models.BooleanField(default=True)
+    # email_address = models.EmailField()
+    # account = models.FloatField()
 
