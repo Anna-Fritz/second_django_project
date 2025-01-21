e7846f4d60b1280cb35e8cb3ab3f1856ce17a5f2
Anna Fritz <anna_eida@gmx.de>
2025-01-21 14:58:57 +0100

    added fields account & email_address to customer

--- sales/models.py
+++ sales/models.py
@@ -6,6 +6,6 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     second_name = models.CharField(max_length=30)
     newsletter_abo = models.BooleanField(default=True)
-    # email_address = models.EmailField()
-    # account = models.FloatField()
+    email_address = models.CharField(max_length=30, blank=True, default="")
+    account = models.FloatField(blank=True, null=True)
 
