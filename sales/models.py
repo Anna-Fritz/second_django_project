fc683776b31c56bbf7d2c247250c5d588aff550b
Anna Fritz <anna_eida@gmx.de>
2025-01-22 12:49:07 +0100

    added more Models in sales & filled db

--- sales/models.py
+++ sales/models.py
@@ -4,8 +4,35 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
-    second_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     newsletter_abo = models.BooleanField(default=True)
     email_address = models.CharField(max_length=30, blank=True, default="")
     account = models.FloatField(blank=True, null=True)
+    # one-to-many Order
 
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+
+
+class Bill(models.Model):
+    total_amount = models.FloatField()
+    is_paid = models.BooleanField(default=False)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    # many-to-one Customer
+
+    products = models.ManyToManyField(Product, through="Producttype")
+    # many-to-many Product
+
+    bill = models.OneToOneField(Bill, on_delete=models.CASCADE)
+    # one-to-one Bill
+
+
+class Producttype(models.Model):
+    type_name = models.CharField(max_length=30)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
