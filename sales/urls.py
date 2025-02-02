c18e5d043db38a3655901f659b1b7954f5746fa6
Anna Fritz <anna_eida@gmx.de>
2025-02-03 00:03:24 +0100

    addedListView in views.py and urls.py

--- sales/urls.py
+++ sales/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import CustomerListView, CustomerListSearchView
 
 urlpatterns = [
-    
+    path('', CustomerListView.as_view()),
+    path('<str:name>/', CustomerListSearchView.as_view())
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Customer
 
 # Create your views here.
+
+
+class CustomerListView(ListView):
+    model = Customer
+    template_name = "sales/list.html"
+
+
+class CustomerListSearchView(CustomerListView):
+    def get_queryset(self):
+        name = self.kwargs.get("name")
+        return Customer.objects.filter(first_name__icontains=name)
