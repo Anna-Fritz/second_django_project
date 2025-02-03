5d62f9f345b6a477125f55a3e81600012568b477
Anna Fritz <anna_eida@gmx.de>
2025-02-03 01:04:35 +0100

    added get_object def to CustomerDetailView, added additional info to detail.html

--- sales/views.py
+++ sales/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Customer
 from django.views.generic.detail import DetailView
+from django.utils import timezone
 
 # Create your views here.
 
@@ -20,3 +21,8 @@ class CustomerListSearchView(CustomerListView):
 class CustomerDetailView(DetailView):
     model = Customer
     template_name = "sales/detail.html"
+
+    def get_object(self):
+        obj = super().get_object()
+        obj.last_accessed = timezone.now()
+        return obj
